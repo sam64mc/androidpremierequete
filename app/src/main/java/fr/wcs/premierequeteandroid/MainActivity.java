300ec251753aeae34d189322b3475e5ce7f2ba0e
sam64mc@gmail.com <johndoe@example.com>
2017-09-08 10:48:25 +0200

    ajout du click sur checkbox

--- app/src/main/java/fr/wcs/premierequeteandroid/MainActivity.java
+++ app/src/main/java/fr/wcs/premierequeteandroid/MainActivity.java
@@ -2,6 +2,10 @@ package fr.wcs.premierequeteandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkboxWilder = (CheckBox) findViewById(R.id.checkbox);
+        checkboxWilder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               EditText editFirstname = (EditText) findViewById(R.id.firstname);
+                EditText editlastname = (EditText)findViewById(R.id.lastname);
+                Button buttonValidate = (Button)findViewById(R.id.button);
+                if (checkboxWilder.isChecked()) {
+                   // est coché
+                    editFirstname.setEnabled(true);
+                    editlastname.setEnabled(true);
+                    buttonValidate.setEnabled(true);
+                } else {
+                    // n'est pas coché
+                    editFirstname.setEnabled(false);
+                    editlastname.setEnabled(false);
+                    buttonValidate.setEnabled(false);
+                }
+            }
+        });
+
+
     }
 }
