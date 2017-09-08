6522c9b88a8b699750d8df1e65efa751a04f5fa4
sam64mc@gmail.com <johndoe@example.com>
2017-09-08 11:38:58 +0200

    ma quete fonctionne

--- app/src/main/java/fr/wcs/premierequeteandroid/MainActivity.java
+++ app/src/main/java/fr/wcs/premierequeteandroid/MainActivity.java
@@ -1,11 +1,14 @@
 package fr.wcs.premierequeteandroid;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final CheckBox checkboxWilder = (CheckBox) findViewById(R.id.checkbox);
+
         checkboxWilder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +38,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button buttonValidate = (Button)findViewById(R.id.button);
+        buttonValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editFirstname = (EditText) findViewById(R.id.firstname);
+                EditText editlastname = (EditText)findViewById(R.id.lastname);
+                if(editFirstname.getText().toString().isEmpty()
+                || editlastname.getText().toString().isEmpty() )
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = getResources().getString(R.string.error);
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast messageErreur = Toast.makeText(context, text, duration);
+                    messageErreur.show(); }
+                else{
+                    TextView textCongratulation = (TextView) findViewById(R.id.congratulation);
+                    textCongratulation.setText(getResources().getString(R.string.congratulation)
+                     + " " + editFirstname.getText().toString()
+                      +" " + editlastname.getText().toString()
+                    );
+
+
+            }
+        }
+             });
+
 
 
     }
